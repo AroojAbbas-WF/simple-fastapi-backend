dd3e91bf4d01916cd3430be1a4fa2a6e80c964cb
AroojAbbas-WF <workf7805@gmail.com>
2026-06-03 10:53:05 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello! My backend is working."}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok", "backend": "running"}
